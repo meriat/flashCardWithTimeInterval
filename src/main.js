fce13ea8805ce857aca6794538a23011f641f7ca
Connor McCarthy and Meria Thomas <connor@>
2018-10-16 11:00:06 -0700

    add gif

--- src/main.js
+++ src/main.js
@@ -6,6 +6,21 @@ import { FlashCard } from './flashCard.js';
 
 
 $(document).ready(function () {
+    $("#waiting").show();
+    $.ajax({
+        url: `https://api.giphy.com/v1/gifs/random?api_key=hjK0Bz4npyo60C0xlHhhwcSqDKMzDkSr&tag=waiting&rating=G`,
+        type: 'GET',
+        data: {
+            format: 'json'
+        },
+        success: function(response){
+            $('#waiting').append(`<img src=${response.data.images.fixed_height_downsampled.url} alt="some random gif">`);
+        },
+        error: function(){
+            $('#errors').text("there was an error processing your request. Please try again!!!!");
+        }
+    });
+
     let newFlashCard = new FlashCard();
     newFlashCard.setTimeLimit();
     setInterval(() => {
@@ -13,8 +28,8 @@ $(document).ready(function () {
     }, 50);
 
     $("#flashCardForm").submit(function (event) {
-        event.preventDefault(); 
-        
+        event.preventDefault();
+
         let result = $('input[name="option"]:checked').val();
         let result2 = $('input[name="option2"]:checked').val();
         let result3 = $('input[name="option3"]:checked').val();
@@ -23,6 +38,5 @@ $(document).ready(function () {
         newFlashCard.userAnswer.push(result3);
         $('#score').empty();
         $("#score").text(newFlashCard.points());
-        // newFlashCard.nextQuestion();
     });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+
 
 module.exports = {
   entry: './src/main.js',
@@ -16,10 +19,11 @@ module.exports = {
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
+    new Dotenv(),
     new HtmlWebpackPlugin({
       title: 'project',
       template: './src/index.html',
-      inject: false
+      inject: 'body'
     })
   ],
   module: {
@@ -46,7 +50,17 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ["babel-loader", "eslint-loader"]
-      }
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,  
+        use: [{
+            loader: 'url-loader',
+            options: { 
+                limit: 8000, // Convert images < 8kb to base64 strings
+                name: 'images/[hash]-[name].[ext]'
+            } 
+        }]
+    } 
     ]
   }
 };
